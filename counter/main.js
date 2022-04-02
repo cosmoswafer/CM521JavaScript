0ab10e89285a637fce7ed6aaba6446241040a654
Jimmy.|\| <jimmy09140456@gmail.com>
2022-04-02 15:39:00 +0800

    Finished the counter app but without jquery

--- counter/main.js
+++ counter/main.js
@@ -1,8 +1,12 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <p>Counter: <span class="counter-number">10</span></p>
-  <button class="plus-btn">+1</button>
-  <button class="minus-btn">-1</button>
-`;
+const number_span = document.querySelector('span.counter-number');
+const plus_button = document.querySelector('button.plus-btn');
+const minus_button = document.querySelector('button.minus-btn');
+
+const countNumber = (n) => {
+  number_span.textContent = Number(number_span.textContent) + n;
+}
+
+plus_button.addEventListener('click', (e) => {countNumber(+1)});
+minus_button.addEventListener('click', (e) => {countNumber(-1)});
