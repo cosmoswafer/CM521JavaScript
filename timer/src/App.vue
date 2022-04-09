7f840b5cce6d7823c6008f899207435460921396
Jimmy.|\| <jimmy09140456@gmail.com>
2022-04-09 15:25:26 +0800

    Finished CSS style and lap function

--- timer/src/App.vue
+++ timer/src/App.vue
@@ -10,6 +10,7 @@
             >
                 {{ run_timer ? 'Lap' : 'Reset' }}
             </button>
+            <span id="brand">Stopwatch</span>
             <button
                 @click="run_timer ? stopTimer() : startTimer()"
                 id="start-and-stop"
@@ -17,12 +18,14 @@
             >
                 {{ run_timer ? 'Stop' : 'Start' }}
             </button>
-            <span id="brand">Stopwatch</span>
         </div>
         <ul id="stopwatch-records">
+            <li v-if="run_timer">
+                <span>Lap</span><span>{{ lap_text }}</span>
+            </li>
             <li v-for="(v, i) in laps" :key="i">
-                <span>Lap {{i+1}}</span>
-                <span>{{v}}</span>
+                <span>Lap {{ i + 1 }}</span>
+                <span>{{ v }}</span>
             </li>
         </ul>
     </div>
@@ -33,21 +36,19 @@ export default {
     data() {
         return {
             time_value: 0,
+            lap_time: 0,
             run_timer: false,
             begin_time: 0,
+            lap_begin: 0,
             laps: [],
         };
     },
     computed: {
         timer_text() {
-            const ts = this.time_value;
-            const ms10 = Math.floor((ts % 1000) / 10);
-            const seconds = Math.floor(ts / 1000) % 60;
-            const minutes = Math.floor(ts / 1000 / 60);
-
-            return `${String(minutes).padStart(2, '0')}:${String(
-                seconds
-            ).padStart(2, '0')}.${String(ms10).padStart(2, '0')}`;
+            return this.ts2Timer(this.time_value);
+        },
+        lap_text() {
+            return this.ts2Timer(this.lap_time);
         },
     },
     methods: {
@@ -62,10 +63,12 @@ export default {
         },
         timerRafInit: function (ts) {
             this.begin_time = ts - this.time_value;
+            this.lap_begin = ts;
             requestAnimationFrame(this.timerRaf);
         },
         timerRaf: function (ts) {
             this.time_value = ts - this.begin_time;
+            this.lap_time = ts - this.lap_begin;
             if (this.run_timer) requestAnimationFrame(this.timerRaf);
         },
         startTimer: function () {
@@ -80,57 +83,97 @@ export default {
             this.laps = [];
         },
         addLap: function () {
-            this.laps.push(this.timer_text);
+            const lap_time_now = this.lap_time;
+            this.lap_begin += this.lap_time; //ts = lap_time + lap_begin
+            this.laps.push(this.ts2Timer(lap_time_now));
         },
     },
 };
 </script>
 
 <style>
-html,
+:root {
+    --seplin-colour: #323234;
+}
+
 body {
-    width: 100%;
-    height: 100%;
-    padding: 0;
+    font-family: monospace;
     margin: 0;
-    font-size: 15px;
+    padding: 0;
 }
 
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: #000;
-    width: 100%;
-    height: 100%;
+    background: black;
+    color: white;
     display: flex;
     flex-direction: column;
-    color: #fff;
+    min-height: 100vh;
+    width: 100%;
 }
 
 #stopwatch {
-    flex: 0px 1 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 5rem;
+    font-size: 7rem;
+    padding: 2em 0;
+    text-align: center;
 }
 
-#stopwatch-records {
-    flex: 0px 1 1;
+#stopwatch-controls {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 32px;
+    text-align: center;
+}
+
+#stopwatch-controls button {
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+    font-size: 2.5rem;
+    height: 3.5em;
+    width: 3.5em;
+    background: var(--background-colour);
+    color: var(--font-colour);
+    outline: 3px solid var(--background-colour);
+    outline-offset: 3px;
+}
+
+#reset-and-lap {
+    --background-colour: #333333;
+    --font-colour: #ffffff;
+}
+
+#start-and-stop {
+    --background-colour: #082a12;
+    --font-colour: #2ed158;
+}
+
+#start-and-stop.red {
+    --background-colour: #320e0b;
+    --font-colour: #ff453a;
 }
 
 #stopwatch-records {
     list-style: none;
     padding: 0;
-    margin: 0 1.2rem;
-    overflow: auto;
 }
 
 #stopwatch-records li {
-    border-bottom: 1px solid #323234;
-    display: flex;
-    padding: 0.8rem 0;
+    border-bottom: 2px solid var(--seplin-colour);
+    display: grid;
+    font-size: 2rem;
+    grid-template-columns: 4fr 6fr;
+    line-height: 2rem;
+    padding: 1rem 2rem;
+}
+
+#stopwatch-records li:first-child {
+    border-top: 2px solid var(--seplin-colour);
+    margin-top: 1em;
+}
+
+#stopwatch-records li span:nth-child(2) {
+    text-align: right;
 }
 
 #stopwatch-records li.green {
@@ -138,61 +181,6 @@ body {
 }
 
 #stopwatch-records li.red {
-    color: #ff453a;
-}
-
-#stopwatch-records li:first-child {
-    border-top: 1px solid #323234;
-}
-
-#stopwatch-records span {
-    display: block;
-    flex: 0px 1 1;
-}
-
-#stopwatch-records span:last-child {
-    text-align: right;
-}
-
-#stopwatch-controls {
-    flex: 80px 0 0;
-    padding: 0.2rem 1.2rem;
-    overflow: hidden;
-    position: relative;
-    margin-bottom: 1.5rem;
-}
-
-#brand {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
-}
-
-button#reset-and-lap,
-button#start-and-stop {
-    width: 80px;
-    height: 80px;
-    outline: none;
-    background-color: #333;
-    border: 2px solid #000;
-    border-radius: 100%;
-    color: #fff;
-    font-size: 1rem;
-    box-shadow: 0px 0px 0px 2px #333;
-    float: left;
-}
-
-button#start-and-stop {
-    background-color: #082a12;
-    box-shadow: 0px 0px 0px 2px #082a12;
-    color: #2ed158;
-    float: right;
-}
-
-button#start-and-stop.red {
-    background-color: #320e0b;
-    box-shadow: 0px 0px 0px 2px #320e0b;
     color: #ff453a;
 }
 </style>
